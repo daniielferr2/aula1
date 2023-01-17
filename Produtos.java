07b8bc23b7d6cf035b6aa81eec8e64360b739841
daniielferr2 <daniielferr@hotmail.com>
2023-01-17 15:17:19 -0300

    Atributo Local criado

--- Produtos.java
+++ Produtos.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package componentes;
 
 /**
@@ -13,6 +10,7 @@ public class Produtos {
     String nome;
     Integer preco;
     String descricao;
+    String local;
     
     public Integer getId() {
         return id;
